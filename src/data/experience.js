49ac5ce08942ca548753d44fe11650478d8c19de
Urva <Urvapatel1@gmail.com>
2023-09-06 22:47:39 -0400

    website

---
+++ src/data/experience.js
@@ -0,0 +1,74 @@
+const experience = [
+    {
+        "className":"vertical-timeline-element--work",
+        "date": "January 2020 - present",
+        "background":"#e9d35b",
+        "color":"#fff",
+        "title": "Full Stack Engineer - IBM",
+        "subtitle": "Markham, ON",
+        "description": "Worked on IBM streams and IBM Data Replication. Made a vsCode extension from Streams using React/Redux and Typescript. Helped the data replication team launch major features by working both in the front end and back end."
+
+    },
+    {
+        "className":"vertical-timeline-element--work",
+        "date": "May 2019 - August 2019",
+        "background":"#e9d35b",
+        "color":"#fff",
+        "title": "Front End Developer Co-op - AutoCorp Technologies",
+        "subtitle": "Ottawa, ON",
+        "description": "Developed www.carloanscanada.com using React, GatsbyJs, and EmotionJs for css"
+
+    },
+    {
+        "className":"vertical-timeline-element--work",
+        "date": "September 2018 - December 2018",
+        "background":"#e9d35b",
+        "color":"#fff",
+        "title": "Software Developer Co-op - Treasury Board of Canada",
+        "subtitle": "Ottawa, ON",
+        "description": "Developed an interactive React/Redux website using .NET CORE 2.1. Created a JavaScript script to automatically extract xml data from forms and append it to an excel sheet"
+
+    },
+    {
+        "className":"vertical-timeline-element--work",
+        "date": "January 2018 - May 2018",
+        "background":"#e9d35b",
+        "color":"#fff",
+        "title": "Front End Developer Co-op - Ribbon Communications",
+        "subtitle": "Ottawa, ON",
+        "description": "Worked on bug fixes for the current release of the Skype for Business web application. Created a tool using React and node.js to automatically create config.json files for the Skype for Business web application"
+
+    },
+    {
+        "className":"vertical-timeline-element--work",
+        "date": "May 2017 - August 2017",
+        "background":"#e9d35b",
+        "color":"#fff",
+        "title": "Fron End Developer Co-op - Espial",
+        "subtitle": "Ottawa, ON",
+        "description": "Developed an interactive HTML5/React prototype using JavaScript, HTML, jQuery, and CSS animations."
+
+    },
+    {
+        "className":"vertical-timeline-element--education",
+        "date": "September 2015 - December 2019",
+        "background":"#3e497a",
+        "color":"#fff",
+        "title": "Computer Engineering with Co-op",
+        "subtitle": "Ottawa, ON",
+        "description": "Studied Computer Engineering which combined elements of both electrical engineering and computer science to provide me with a comprehensive understanding of computer systems."
+
+    },
+    {
+        "className":"vertical-timeline-element--education",
+        "date": "September 2012 - April 2015",
+        "background":"#3e497a",
+        "color":"#fff",
+        "title": "Central Secondary School",
+        "subtitle": "London, ON",
+        "description": ""
+
+    }
+]
+
+export default experience;
